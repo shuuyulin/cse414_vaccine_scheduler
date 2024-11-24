0259bcb35ad1fc34c5253e3e6f1bb3e0fd73afd3
林書宇 <linshuyu@linshuyudebijixingdiannao.local>
2024-11-24 00:26:15 -0800

    Part I: database design and environment setup

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,18 +1,39 @@
-CREATE TABLE Caregivers (
+CREATE TABLE Caregivers
+(
     Username varchar(255),
     Salt BINARY(16),
     Hash BINARY(16),
     PRIMARY KEY (Username)
 );
 
-CREATE TABLE Availabilities (
+CREATE TABLE Availabilities
+(
     Time date,
     Username varchar(255) REFERENCES Caregivers,
     PRIMARY KEY (Time, Username)
 );
 
-CREATE TABLE Vaccines (
+CREATE TABLE Vaccines
+(
     Name varchar(255),
     Doses int,
     PRIMARY KEY (Name)
+);
+
+CREATE TABLE Patients
+(
+    Username varchar (255),
+    Salt BINARY (16),
+    Hash BINARY (16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Reservations
+(
+    Id int,
+    Time date REFERENCES Availabilities,
+    Cusername varchar(255) REFERENCES Availabilities (Username),
+    Pusername varchar(255) REFERENCES Patients (Unsername),
+    Vname varchar(255) REFERENCES Vaccinies (name),
+    PRIMARY KEY (Id)
 );
